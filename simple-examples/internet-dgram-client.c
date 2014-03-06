0618ba27a6d8238a0b35d8cfbd38330e8adf7492
Scott Kuhl <kuhl@mtu.edu>
2014-03-06 11:10:44 -0500

    add udp example

---
+++ simple-examples/internet-dgram-client.c
@@ -0,0 +1,68 @@
+/* Based on: http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html#datagram (code from "Beej's Guide to Network Programming" is public domain).
+ */
+// Modifications by: Scott Kuhl
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define SERVERPORT "4950"    // the port users will be connecting to
+
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+
+    if (argc != 2) {
+	    fprintf(stderr,"usage: %s hostname\n", argv[0]);
+        exit(1);
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and make a socket
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("socket");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "talker: failed to bind socket\n");
+        return 2;
+    }
+
+    if ((numbytes = sendto(sockfd, "hello world", strlen("hello world"), 0,
+             p->ai_addr, p->ai_addrlen)) == -1) {
+        perror("sendto");
+        exit(1);
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("sent %d bytes to %s\n", numbytes, argv[1]);
+    close(sockfd);
+
+    return 0;
+}
