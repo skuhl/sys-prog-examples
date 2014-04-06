6564154745e648fda397b3cacd9b21943cd471cd
Scott Kuhl <kuhl@mtu.edu>
2014-04-06 17:27:37 -0400

    add another example in modify-string-literal.c

--- simple-examples/modify-string-literal.c
+++ simple-examples/modify-string-literal.c
@@ -17,16 +17,29 @@ int main(void)
 	array[0] = 'X';
 	printf("%s\n", array);
 
-	// String literals are global and we shouldn't change them. For
-	// example, if the same string literal appeared multiple times in
-	// your program, the compiler may only put a single global string
-	// in your code and refer to that string from multiple places.:
-	char *literal = "Hello world";
-	// A better way to declare this variable that would reduce the
-	// chances of you making a mistake:
-	// const char *literal = "Hello world";
-	*literal = 'X'; // This WON'T WORK because we are changing the string literal itself!
-	printf("%s\n", literal);
+	if( 0 ) // CHANGE ME TO 1 to see the error this code causes!
+	{
+		// String literals are global and we shouldn't change them. For
+		// example, if the same string literal appeared multiple times in
+		// your program, the compiler may only put a single global string
+		// in your code and refer to that string from multiple places.:
+		char *literal = "Hello world";
+		// A better way to declare this variable that would reduce the
+		// chances of you making a mistake:
+		// const char *literal = "Hello world";
+		*literal = 'X'; // This WON'T WORK because we are changing the string literal itself!
+		printf("%s\n", literal);
+	}
 
+	if( 0 ) // CHANGE ME TO 1 to see the error this code causes!
+	{
+		char *mallocStr = (char*) malloc(256);
+		// the following line makes the mallocStr pointer point to a
+		// string literal. We shouldn't change or free() it.
+		mallocStr = "Hello world";
+		free(mallocStr); // WRONG!
+	}
+
+	
 	return 0;
 }
