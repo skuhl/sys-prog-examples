f564a93ede7fddd685bdeb4f0a42787a74d4be8e
Scott Kuhl <kuhl@mtu.edu>
2014-11-19 15:20:18 -0500

    Check for __linux__ in float-except.c

--- simple-examples/float-except.c
+++ simple-examples/float-except.c
@@ -12,8 +12,10 @@ int main(void)
 
 	// Turns on SIGFPE signal for divide by zero, overflow, underflow,
 	// etc.
+#ifdef __linux__
 	feenableexcept(FE_ALL_EXCEPT); // Linux only!
-
+#endif
+	
 	printf("Dividing by zero (second time)\n");
 	c = a/b;
 	printf("Result: %f (we shouldn't get here)\n", c);
