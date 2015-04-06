9be48d9b32ceafa50010c48488fef377a644b8f4
Scott Kuhl <kuhl@mtu.edu>
2015-04-06 13:00:28 -0400

    add static variable example

---
+++ simple-examples/static.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Static functions and static global variables are only visible in
+ * the file that it is declared in. Static variables inside of a
+ * function have a different meaning and is described below */
+
+void foo()
+{
+	int x = 0;
+	static int staticx = 0; // initialized once; keeps value between invocations of foo().
+	x++;
+	staticx++;
+	printf("x=%d\n", x);
+	printf("staticx=%d\n", staticx);
+}
+
+int main()
+{
+	for(int i=0; i<10; i++)
+		foo();
+}
