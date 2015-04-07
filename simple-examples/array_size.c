c2fa71cdb8b7ec64d0c55fef1aa32345f9618a1e
Scott Kuhl <kuhl@mtu.edu>
2015-04-07 10:13:16 -0400

    add ARRAY_SIZE example

---
+++ simple-examples/array_size.c
@@ -0,0 +1,36 @@
+// Scott Kuhl
+#include <stdio.h>
+
+/* We can figure out how many items are in an array with this
+ * macro. 'x' is the array we want the length of. The size of the
+ * entire array divided by the size of the individual items in the
+ * array tell us how many items are in the array.
+ *
+ * WARNING: This won't work universally. See the caveat function
+ * below. */
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
+
+void caveat(int three[], char hello[])
+{
+	printf("Caveat:\n");
+	/* The behavior of sizeof() chances when it is called on an array
+	 * that is passed as a parameter. sizeof(three) and sizeof(hellow)
+	 * will not be the size of a pointer on this machine. */
+	printf("items in three: %d / %d = %d\n", (int) sizeof(three), (int) sizeof(three[0]), (int) ARRAY_SIZE(three));
+	printf("items in hello: %d / %d = %d\n", (int) sizeof(hello), (int) sizeof(hello[0]), (int) ARRAY_SIZE(hello));
+}
+
+
+int main(void)
+{
+	int three[3] = { 1, 2, 3 };
+	char hello[5] = "hello";
+
+	printf("items in three: %d / %d = %d\n", (int) sizeof(three), (int) sizeof(three[0]), (int) ARRAY_SIZE(three));
+	printf("items in hello: %d / %d = %d\n", (int) sizeof(hello), (int) sizeof(hello[0]), (int) ARRAY_SIZE(hello));
+	caveat(three, hello);
+}
+
+
+
