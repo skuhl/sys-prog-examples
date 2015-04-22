a1b3972354fdb4343f0359d05bb86fa290b74a5d
Scott Kuhl <kuhl@mtu.edu>
2015-04-21 21:44:05 -0400

    add comments about additional endianness functions

--- simple-examples/endianness.c
+++ simple-examples/endianness.c
@@ -110,6 +110,26 @@ int main(void)
 	// uint32_t ntohl(uint32_t netlong);
 	// uint16_t ntohs(uint16_t netshort);
 
+	// Host to big endian
+	// uint16_t htobe16(uint16_t host_16bits);
+	// uint32_t htobe32(uint32_t host_32bits);
+	// uint64_t htobe64(uint64_t host_64bits);
+
+	// Host to little endian
+	// uint16_t htole16(uint16_t host_16bits);
+	// uint32_t htole32(uint32_t host_32bits);
+	// uint64_t htole64(uint64_t host_64bits);
+
+	// Big endian to the endianness of the host
+	// uint16_t be16toh(uint16_t big_endian_16bits);
+	// uint32_t be32toh(uint32_t big_endian_32bits);
+	// uint64_t be64toh(uint64_t big_endian_64bits);
+
+	// Little endian to the endianness of the host
+	// uint16_t le16toh(uint16_t little_endian_16bits);
+	// uint32_t le32toh(uint32_t little_endian_32bits);
+ 	// uint64_t le64toh(uint64_t little_endian_64bits);
+	
 	printf("\n");
 	printf("We created the file '%s'. You can safely delete it.", FILENAME);
 }
