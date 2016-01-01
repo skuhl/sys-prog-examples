cea41912b5b44677a74025e4395c0760740339b6
Scott Kuhl <kuhl@mtu.edu>
2016-01-01 17:16:33 -0500

    add example for using pause()

---
+++ simple-examples/pause.c
@@ -0,0 +1,40 @@
+// Scott Kuhl
+
+/* Illustrates how to use pause() */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+
+
+void sighandler(int signo)
+{
+	// See signal.c and signal-better.c for a more comprehensive
+	// example of handling signals.
+	printf("In signal handler.\n");
+}
+
+int main()
+{
+	printf("Pausing until we receive a signal.\n");
+	printf("This process has pid %d\n", getpid());
+
+	signal(SIGUSR1,  sighandler); // set up a signal handler
+	
+	/* Pause causes us to sleep until...:
+
+	   - LINUX: "a signal is delivered that either terminates the
+	   process or causes the invocation of a signal catching function"
+
+	   - OSX: pause() returns "upon termination of a signal handler
+         that is started during a pause()."
+	*/
+	pause(); // it is better to use sigsuspend() instead, see sigsuspend.c
+
+	/* For a description of why pause() is not ideal, see:
+	   http://stackoverflow.com/questions/6328055/whats-the-problem-of-pause-at-all
+	*/
+
+	printf("Returned from pause(), exiting.\n");
+	return 0;
+}
