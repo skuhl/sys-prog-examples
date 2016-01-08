fbf0d552e9a0e8d4202e95ad15f654a85476f33f
Scott Kuhl <kuhl@mtu.edu>
2016-01-08 14:48:29 -0500

    improve comments in vla.c

--- simple-examples/vla.c
+++ simple-examples/vla.c
@@ -15,14 +15,22 @@ void fixedArray()
    compiler warn you about VLAs with the '-Wvla' compiler flag.
 
    One problem with VLAs is that if the array is too big, the behavior
-   of your program may be undefined. Setting a variable length array
-   to a length of 0 or a negative number may also be undefined. For
-   more information, see:
+   of your program may be undefined. For example, gcc allocates memory
+   for VLAs on the stack---and allocating too much can overflow the
+   stack. Setting a variable length array to a length of 0 or a
+   negative number may also be undefined. For more information, see:
    https://www.securecoding.cert.org/confluence/display/c/ARR32-C.+Ensure+size+arguments+for+variable+length+arrays+are+in+a+valid+range
+
+   Use malloc() to allocate large amounts of memory. The return value
+   will tell you if it succeed or not (instead of a VLA which might
+   crash your program if it fails).
+   
 */
 void variableLengthArray(int n)
 {
-	char vla[n]; // the compiler doesn't know how long VLA is until run-time. Therefore, this is a variable length array (VLA).
+	char vla[n]; // the compiler doesn't know how long VLA is until
+				 // run-time. Therefore, this is a variable length
+				 // array (VLA).
 	
 	for(int i=0; i<n; i++)
 		vla[i]='X';
