b6a990b999bef014e049d9028548430619b6bc8b
Scott Kuhl <kuhl@mtu.edu>
2016-01-12 10:06:32 -0500

    update comments in exitstatus.c

--- simple-examples/exitstatus.c
+++ simple-examples/exitstatus.c
@@ -10,12 +10,21 @@
    Also note that if a program is killed because of a signal, the exit
    status will be 128+N where N is the number of that signal. SIGINT=2
    is what happens when you press Ctrl+C. So every program that you
-   Ctrl+C to exit should exit with the exit status of 128+2 = 130
+   Ctrl+C to exit should exit with the exit status of 128+2 = 130.
+
+   The glibc manual recommends avoiding use of exit status values of
+   128 or higher:
+
+     "A general convention reserves status values 128 and up for
+     special purposes. In particular, the value 128 is used to
+     indicate failure to execute another program in a subprocess. This
+     convention is not universally obeyed, but it is a good idea to
+     follow it in your programs."
+     - https://www.gnu.org/software/libc/manual/html_node/Exit-Status.html
    
    Note: You can also set the exit status by calling exit() and using
    the status code as the parameter.
  */
-
 #include <stdio.h>
 
 int main(int argc, char *argv[])
@@ -30,6 +39,6 @@ int main(int argc, char *argv[])
 	}
 
 	printf("Returning %d\n", exitStatus);
-	printf("If you are using a bash-like shell, try running 'echo $?' immediately after you run this program.\n");
+	printf("If you are using a bash-like shell, try running 'echo $?' immediately after you run this program to see the actual return value.\n");
 	return exitStatus;
 }
