617e22b332c13180416e949a084bf5865d8007b5
Scott Kuhl <kuhl@mtu.edu>
2016-01-12 10:03:57 -0500

    add bitwise operator example

---
+++ simple-examples/bitwise.c
@@ -0,0 +1,70 @@
+// Scott Kuhl
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+	  char is always a byte in C. However, it may be signed, unsigned
+	  or neither (i.e., unspecified). See char-signed-unsigned.c
+	*/
+	unsigned char a = 0x33; // 00110011 in binary;
+	unsigned char b = 0x0F; // 00001111 in binary;
+
+	// Values that we would expect
+	unsigned char AandB = 0x03;
+	unsigned char AorB  = 0x3F;
+	unsigned char AxorB = 0x3C;
+	unsigned char notA  = 0xCC;
+	unsigned char notB  = 0xF0;
+	if((unsigned char) (a & b) != AandB) printf("AND Fail!\n");
+	if((unsigned char) (a | b) != AorB) printf("OR Fail!\n");
+	if((unsigned char) (a ^ b) != AxorB) printf("XOR Fail!\n");
+	if((unsigned char) (~a) != notA) printf("NOT A Fail!\n");
+	if((unsigned char) (~b) != notB) printf("NOT B Fail!\n");
+
+	/* Why are the casts above necessary? C will cast integer types
+	 * that are smaller than a char to an integer. Without the cast,
+	 * some compilers will sometimes complain about comparison between
+	 * different types. See integer-promotion.c */
+
+
+	/* This...      --> can be written like this:
+	   a = a & b    -->  a &= b
+	   a = a | b    -->  a |= b
+	   a = a ^ b    -->  a ^= b
+	*/
+
+	// Use << to shift bits to the left. After the bits are shifted,
+	// any blank bits are set to 0.
+	unsigned char allOn = 0xFF;
+	if( (unsigned char) (allOn << 1) != 0xFE ) printf("<< Fail!");
+	if( (unsigned char) (allOn >> 4) != 0x0F ) printf("<< Fail!");
+
+
+	// You can shift and assign at the same time:
+	unsigned char tmp = 0xFF;
+	tmp <<= 1;    // stores 0xFE in tmp
+	
+
+
+	/* Bitshifts on signed integers may behave differently depending
+	   on implementation. For example, when the variable type is
+	   signed and when the most significant bit is 1. In this case,
+	   when you shift the bits to the right, the new bits will be 1
+	   (according to the GCC compiler) or 0 (according to the
+	   Microsoft C compiler).
+
+	   The C11 standard explains that bitshifts are
+	   implementation-defined and have undefined aspects for signed
+	   types.
+	   https://www.securecoding.cert.org/confluence/display/c/INT13-C.+Use+bitwise+operators+only+on+unsigned+operands
+	*/
+	signed int allOnSigned = -1;
+	printf("Bitshift right on a signed type keeps the most left bit...\n");
+	if( (allOnSigned >> 1) == -1 )
+		printf("...set to one.\n");
+	else if( (allOnSigned >> 1) == 0x7F )
+		printf("...set to zero.\n");
+	else
+		printf("huh?\n");
+}
