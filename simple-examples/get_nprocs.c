a1dd462a7b0bdc66dc5850c219a3400395e9be63
Scott Kuhl <kuhl@mtu.edu>
2016-01-12 19:39:15 -0500

    fix get_nprocs.c on OSX

--- simple-examples/get_nprocs.c
+++ simple-examples/get_nprocs.c
@@ -1,11 +1,24 @@
 // Scott Kuhl
+
 #include <stdio.h>
+
+#ifdef __linux__
+
 #include <sys/sysinfo.h>
-
-
 int main(void)
 {
 	printf("get_nprocs_conf() returns %d\n", get_nprocs_conf());
 	printf("get_nprocs() returns %d\n", get_nprocs());
 	return 0;
 }
+
+
+#else
+
+int main(void)
+{
+	printf("This example only works on Linux\n");
+	return 0;
+}
+
+#endif
