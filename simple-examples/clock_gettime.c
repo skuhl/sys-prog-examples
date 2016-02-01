688d2ae63f449f729ae80a16f90171d3d0d5091f
Scott Kuhl <kuhl@mtu.edu>
2016-02-01 10:10:29 -0500

    add rdtsc example, update gettime example

--- simple-examples/clock_gettime.c
+++ simple-examples/clock_gettime.c
@@ -10,6 +10,12 @@
 #include <time.h>
 #include <unistd.h>
 
+void print_timespec(struct timespec diff, const char *name)
+{
+	printf("%s.tv_sec=%ld  %s.tv_nsec=%ld\n", name, diff.tv_sec, name, diff.tv_nsec);
+}
+
+
 int main(void)
 {
 #ifdef __linux__
@@ -18,9 +24,24 @@ int main(void)
 	sleep(5);
 	clock_gettime(CLOCK_MONOTONIC, &t2);
 
+	print_timespec(t1, "t1");
+	print_timespec(t2, "t2");
+
+
 	struct timespec diff;
-	if(t2.tv_nsec-t1.tv_nsec < 0)
+	if(t2.tv_nsec < t1.tv_nsec)
 	{
+		/* If nanoseconds in t1 are larger than nanoseconds in t2, it
+		   means that something like the following happened:
+		   t1.tv_sec = 1000    t1.tv_nsec = 100000
+		   t2.tv_sec = 1001    t2.tv_nsec = 10
+
+		   In this case, less than a second has passed but subtracting
+		   the tv_sec parts will indicate that 1 second has passed. To
+		   fix this problem, we subtract 1 second from the elapsed
+		   tv_sec and add one second to the elapsed tv_nsec. See
+		   below:
+		*/
 		diff.tv_sec  = t2.tv_sec  - t1.tv_sec  - 1;
 		diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
 	}
@@ -30,8 +51,9 @@ int main(void)
 		diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
 	}
 
-	double secondsElapsed = diff.tv_sec + diff.tv_nsec / 1000000000.0;
-	printf("Time elapsed (in seconds): %f\n", secondsElapsed);
+	printf("Time elapsed   (float): %.15f seconds\n", diff.tv_sec + (double) diff.tv_nsec / 1000000000L);
+	printf("Time elapsed (integer): %5ld seconds plus %12ld nanoseconds\n", diff.tv_sec, diff.tv_nsec);
+	printf("                  Note: %5ld second   is  %12ld nanoseconds\n", 1L, 1000000000L);
 #else
 	printf("clock_gettime() works only on Linux\n");
 #endif
---
+++ simple-examples/rdtsc.c
@@ -0,0 +1,131 @@
+// Scott Kuhl
+
+/*
+ ReaD Time Stamp Counter (RDTSC) instruction. Returns number of
+ clock cycles since boot. Should work on recent 64 bit machines.
+
+ A more thorough discussion can be found in "How to Benchmark Code
+ Execution Times on Intel IA-32 and IA-64 Instruction Set
+ Architectures" by Gabriele Paoloni (Intel):
+http://www.intel.com/content/www/xr/en/intelligent-systems/embedded-systems-training/ia-32-ia-64-benchmark-code-execution-paper.html
+
+Compiled program can be inspected with "objdump -d rdtsc"
+ 
+ POTENTIAL PROBLEMS:
+
+ - Frequency changes: Some processors change frequencies depending on
+   load for power-saving reasons. Usually this can be prevented with a
+   setting on your OS. "cat /proc/cpuinfo" can tell you the current
+   frequency of your processor.
+
+ - Cache: If there is a cache miss, your code will take more cycles to
+   execute. Running your test multiple times may help.
+
+ - Out of order instructions: The cpuid instruction can force all
+   previous instructions to finish.
+
+ - Context switches: The counter will increase when other processes
+   are switched in. Problem can be minimized by reducing number of
+   instructions between start/stop or by setting the process priority
+   to the highest possible priority.
+
+ - Multi-core processors: The counters aren't synchronized across
+   cores. If your process switches cores, your values may not make
+   sense. The process affinity can be to prevent process migration
+   between cores/processors.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+
+
+// Use the preprocessor so we know definitively that these are placed inline
+#define RDTSC_START(cycles)      \
+	__asm__ volatile("CPUID\n\t" \
+	                 "RDTSC\n\t" \
+	                 "mov %%edx, %0\n\t" \
+	                 "mov %%eax, %1\n\t" \
+	                 : "=r" (start_hi), "=r" (start_lo) \
+	                 :: "%rax", "%rbx", "%rcx", "%rdx");
+
+#define RDTSC_STOP(cycles)        \
+	__asm__ volatile("RDTSCP\n\t" \
+	                 "mov %%edx, %0\n\t" \
+	                 "mov %%eax, %1\n\t" \
+	                 "CPUID\n\t" \
+	                 : "=r" (end_hi), "=r" (end_lo) \
+	                 :: "%rax", "%rbx", "%rcx", "%rdx");
+
+// Returns the elapsed time given the high and low bits of the start and stop time.
+uint64_t elapsed(uint32_t start_hi, uint32_t start_lo,
+                 uint32_t end_hi,   uint32_t end_lo)
+{
+	uint64_t start = (((uint64_t)start_hi) << 32) | start_lo;
+	uint64_t end   = (((uint64_t)end_hi)   << 32) | end_lo;
+	return end-start;
+}
+
+int main(void)
+{
+	uint32_t end_hi, end_lo;
+	uint32_t start_hi, start_lo; 
+
+	RDTSC_START();
+	sleep(1);
+	RDTSC_STOP();
+	printf("elapsed: %ld (sleep(1))\n", elapsed(start_hi, start_lo, end_hi, end_lo));
+	printf("\n\n\n");
+
+
+	// For the rest of our tests, lets use loops to get more accurate numbers.
+
+#define REPEAT 100
+	
+	uint64_t totalTime = 0;
+	for(int i=0; i<REPEAT; i++)
+	{
+		RDTSC_START();
+		printf("printing!\n"); // how fast is printf()?
+		RDTSC_STOP();
+		uint64_t e = elapsed(start_hi, start_lo, end_hi, end_lo);
+		printf("trial %d: %ld (printf)\n", i, e);
+		totalTime += e;
+	}
+	printf("average: %f\n", totalTime/(float)REPEAT);
+
+	
+	printf("\n\n\n");
+	totalTime = 0;
+	for(int i=0; i<REPEAT; i++)
+	{
+		RDTSC_START();
+		// how fast is nothing at all?
+		RDTSC_STOP();
+	    uint64_t e = elapsed(start_hi, start_lo, end_hi, end_lo);
+		printf("trial %d: %ld (NOTHING)\n", i, e);
+		totalTime += e;
+	}
+	printf("average: %f\n", totalTime/(float)REPEAT);
+	printf("\n\n\n");
+
+	totalTime = 0;
+	for(int i=0; i<REPEAT; i++)
+	{
+		volatile int var = 0;
+		int k=0; 
+		RDTSC_START();
+		// how fast is a loop that we can choose how many times it runs?
+		for(; k<2; k++) // Change how many times this loop runs, see what happens.
+			(var) = 1;
+		RDTSC_STOP();
+		uint64_t e = elapsed(start_hi, start_lo, end_hi, end_lo);
+		printf("trial %d: %ld (loop)\n", i, e);
+		totalTime += e;
+	}
+	printf("average: %f\n", totalTime/(float)REPEAT);
+
+
+	return 0;
+}
