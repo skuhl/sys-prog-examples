46a480da0d02d58a488378cba8a0b54c2cbe3689
Scott Kuhl <kuhl@mtu.edu>
2016-03-17 20:08:15 -0400

    update abort.c

--- simple-examples/abort.c
+++ simple-examples/abort.c
@@ -1,4 +1,9 @@
 // Scott Kuhl
+
+/* abort() allows you to exit your program. It is typically used if
+   your program should exit because of an unusual error. It will send
+   the SIGABRT signal to your process.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
